c271b90bdf62cbefe2fbf8b2cfaf24ed5c5f7733
Lucille865 <lucille.bon007@gmail.com>
2025-01-29 12:45:46 +0100

    noeud

--- Pb scientifique/Pb scientifique/Noeud.cs
+++ Pb scientifique/Pb scientifique/Noeud.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace Pb_scientifique
 {
-    internal class Noeud
+    public class Noeud
     {
+        public int numero;
+        public List<Noeud> voisins;
 
+        public Noeud(int numero)
+        {
+            this.numero = numero;
+            this.voisins = new List<Noeud>();
+        }
+
+        public int Numero
+        {get { return numero; }}
+
+        public List<Noeud> Voisins 
+        { get { return voisins; } }
+
+        public void AjouterVoisins(Noeud voisin)
+        {
+            if (!Voisins.Contains(voisin)){
+                Voisins.Add(voisin);
+            }
+        }
+
+        public string toString()
+        {
+            return "Membre "+this.numero;
+        }
     }
 }
