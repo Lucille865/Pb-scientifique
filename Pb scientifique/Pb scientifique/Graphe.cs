1ddfc1f9cc3636367ca99c14a8f720ddcc5b936c
fl00re <flore.auffret@gmail.com>
2025-02-05 09:43:05 +0100

    yeaahhh

--- Pb scientifique/Pb scientifique/Graphe.cs
+++ Pb scientifique/Pb scientifique/Graphe.cs
@@ -6,123 +6,168 @@ using System.Threading.Tasks;
 
 namespace Pb_scientifique
 {
-    internal class Graphe
-    {
-        private List<int> noeuds;
-        private List<Lien> liens;
-        private Dictionary<int, List<int>> listeAdjacence;
-        private int[,] matriceAdjacence;
-
-        public Graphe(int nbSommets)
+        public class Graphe
         {
-            noeuds = new List<int>();
-            liens = new List<Lien>();
-            listeAdjacence = new Dictionary<int, List<int>>();
-            matriceAdjacence = new int[nbSommets, nbSommets];
+            private List<Noeud> noeuds;
+            private List<Lien> liens;
 
-            // Création des sommets
-            for (int i = 1; i <= nbSommets; i++)
+            public Graphe()
             {
-                noeuds.Add(i);
-                listeAdjacence[i] = new List<int>(); // Chaque sommet a une liste de voisins
+                noeuds = new List<Noeud>();
+                liens = new List<Lien>();
             }
-        }
 
-        public void AjouterLien(int num1, int num2)
-        {
-            if (listeAdjacence.ContainsKey(num1) && listeAdjacence.ContainsKey(num2))
+            public void AjouterNoeud(int numero)
             {
-                listeAdjacence[num1].Add(num2);
-                listeAdjacence[num2].Add(num1);
-
-                matriceAdjacence[num1 - 1, num2 - 1] = 1;
-                matriceAdjacence[num2 - 1, num1 - 1] = 1;
+                Noeud nouveauNoeud = new Noeud(numero);
+                noeuds.Add(nouveauNoeud);
             }
-        }
 
-        public void AfficherListeAdjacence()
-        {
-            Console.WriteLine("Liste d'adjacence :");
-            foreach (var entry in listeAdjacence)
+            public void AjouterLien(int num1, int num2)
             {
-                Console.Write(entry.Key + " -> ");
-                foreach (var voisin in entry.Value) // Utilisation directe de la liste d'adjacence
+                Noeud noeud1 = null;
+                Noeud noeud2 = null;
+
+                foreach (var noeud in noeuds)
                 {
-                    Console.Write(voisin + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        public void AfficherMatriceAdjacence()
-        {
-            Console.WriteLine("\nMatrice d'adjacence :");
-            for (int i = 0; i < matriceAdjacence.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriceAdjacence.GetLength(1); j++)
-                {
-                    Console.Write(matriceAdjacence[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        public void ParcoursEnLargeur(int debut)
-        {
-            Noeud startNode = noeuds.Find(n => n.Numero == debut);
-            if (startNode == null) return;
-
-            bool[] visite = new bool[noeuds.Count];
-            Queue<Noeud> file = new Queue<Noeud>();
-            file.Enqueue(startNode);
-            visite[startNode.Numero - 1] = true;
-
-            Console.WriteLine("\nParcours en largeur à partir du sommet " + debut + " :");
-            while (file.Count > 0)
-            {
-                Noeud courant = file.Dequeue();
-                Console.Write(courant.Numero + " ");
-
-                foreach (var voisin in courant.Voisins)
-                {
-                    if (!visite[voisin.Numero - 1])
+                    if (noeud.Numero == num1)
                     {
-                        file.Enqueue(voisin);
-                        visite[voisin.Numero - 1] = true;
+                        noeud1 = noeud;
+                    }
+                    if (noeud.Numero == num2)
+                    {
+                        noeud2 = noeud;
                     }
                 }
-            }
-        }
 
-        // Parcours en profondeur (DFS)
-        public void ParcoursEnProfondeur(int debut)
-        {
-            Noeud startNode = noeuds.Find(n => n.Numero == debut);
-            if (startNode == null) return;
-
-            bool[] visite = new bool[noeuds.Count];
-            Stack<Noeud> pile = new Stack<Noeud>();
-            pile.Push(startNode);
-
-            Console.WriteLine("\nParcours en profondeur à partir du sommet " + debut + " :");
-            while (pile.Count > 0)
-            {
-                Noeud courant = pile.Pop();
-
-                if (!visite[courant.Numero - 1])
+                if (noeud1 != null && noeud2 != null)
                 {
+                    Lien lien = new Lien(noeud1, noeud2);
+                    liens.Add(lien);
+                }
+            }
+
+            public void AfficherListeAdjacence()
+            {
+                Console.WriteLine("Liste d'adjacence :");
+                foreach (var noeud in noeuds)
+                {
+                    Console.Write(noeud.Numero + " -> ");
+                    foreach (var voisin in noeud.Voisins)
+                    {
+                        Console.Write(voisin.Numero + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            public void AfficherMatriceAdjacence()
+            {
+                int n = noeuds.Count;
+                int[,] matrice = new int[n, n];
+
+                foreach (var lien in liens)
+                {
+                    int i = lien.Noeud1.Numero - 1;
+                    int j = lien.Noeud2.Numero - 1;
+                    matrice[i, j] = 1;
+                    matrice[j, i] = 1;
+                }
+
+                Console.WriteLine("\nMatrice d'adjacence :");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(matrice[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            public void ParcoursEnLargeur(int debut)
+            {
+                Noeud startNode = null;
+
+                foreach (var noeud in noeuds)
+                {
+                    if (noeud.Numero == debut)
+                    {
+                        startNode = noeud;
+                        break;
+                    }
+                }
+
+                if (startNode == null)
+                {
+                    Console.WriteLine("Le nœud de départ n'existe pas.");
+                    return;
+                }
+
+                bool[] visite = new bool[noeuds.Count];
+                Queue<Noeud> file = new Queue<Noeud>();
+                file.Enqueue(startNode);
+                visite[startNode.Numero - 1] = true;
+
+                Console.WriteLine("\nParcours en largeur à partir du sommet " + debut + " :");
+                while (file.Count > 0)
+                {
+                    Noeud courant = file.Dequeue();
                     Console.Write(courant.Numero + " ");
-                    visite[courant.Numero - 1] = true;
+
+                    foreach (var voisin in courant.Voisins)
+                    {
+                        if (!visite[voisin.Numero - 1])
+                        {
+                            file.Enqueue(voisin);
+                            visite[voisin.Numero - 1] = true;
+                        }
+                    }
+                }
+            }
+
+            public void ParcoursEnProfondeur(int debut)
+            {
+                Noeud startNode = null;
+
+                foreach (var noeud in noeuds)
+                {
+                    if (noeud.Numero == debut)
+                    {
+                        startNode = noeud;
+                        break;
+                    }
                 }
 
-                foreach (var voisin in courant.Voisins)
+                if (startNode == null)
                 {
-                    if (!visite[voisin.Numero - 1])
+                    Console.WriteLine("Le nœud de départ n'existe pas.");
+                    return;
+                }
+
+                bool[] visite = new bool[noeuds.Count];
+                Stack<Noeud> pile = new Stack<Noeud>();
+                pile.Push(startNode);
+
+                Console.WriteLine("\nParcours en profondeur à partir du sommet " + debut + " :");
+                while (pile.Count > 0)
+                {
+                    Noeud courant = pile.Pop();
+
+                    if (!visite[courant.Numero - 1])
                     {
-                        pile.Push(voisin);
+                        Console.Write(courant.Numero + " ");
+                        visite[courant.Numero - 1] = true;
+                    }
+
+                    foreach (var voisin in courant.Voisins)
+                    {
+                        if (!visite[voisin.Numero - 1])
+                        {
+                            pile.Push(voisin);
+                        }
                     }
                 }
             }
         }
-    }
 }
