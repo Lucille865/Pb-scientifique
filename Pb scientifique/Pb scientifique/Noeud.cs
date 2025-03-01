9a7f47dacb21990a006cbfeb0d7190b9e01abfd7
Lucille865 <lucille.bon007@gmail.com>
2025-03-01 19:25:34 +0100

    Update Noeud.cs

--- Pb scientifique/Pb scientifique/Noeud.cs
+++ Pb scientifique/Pb scientifique/Noeud.cs
@@ -1,44 +1,45 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    namespace Pb_scientifique
+namespace Pb_scientifique
+{
+    public class Noeud
     {
-        public class Noeud
+        public int numero;
+        public List<Noeud> voisins;
+
+        public Noeud(int numero)
         {
-            public int numero;
-            public List<Noeud> voisins;
-
-            public Noeud(int numero)
-            {
-                this.numero = numero;
-                this.voisins = new List<Noeud>();
-            }
-
-            public int Numero
-            {get { return numero; }}
-
-            public List<Noeud> Voisins 
-            { get { return voisins; } }
-
-            public void AjouterVoisins(Noeud voisin)
-            {
-                if (!Voisins.Contains(voisin)){
-                    Voisins.Add(voisin);
-                }
-            }
-
-            public string toString()
-            {
-                    return Numero.ToString();
-                }
-
-            public List<Noeud> GetVoisins()
-            {
-                return voisins;
-            }
-
+            this.numero = numero;
+            this.voisins = new List<Noeud>();
         }
+
+        public int Numero
+        { get { return numero; } }
+
+        public List<Noeud> Voisins
+        { get { return voisins; } }
+
+        public void AjouterVoisins(Noeud voisin)
+        {
+            if (!Voisins.Contains(voisin))
+            {
+                Voisins.Add(voisin);
+            }
+        }
+
+        public string toString()
+        {
+            return Numero.ToString();
+        }
+
+        public List<Noeud> GetVoisins()
+        {
+            return voisins;
+        }
+
     }
+}
